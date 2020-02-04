e18bd83db89098862846bc8351240041ac630f46
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2020-02-04 20:13:08 +0100

    `CGSize` traducido a `CGMedidas` (por ahora)

---
+++ Sources/SwiftUI_EnEspanol/CGMedidas_EnEspanol.swift
@@ -0,0 +1,13 @@
+//
+//  CGMedidas_EnEspanol.swift
+//  
+//
+//  Created by Jeremy Bannister on 2/4/20.
+//
+
+public typealias CGMedidas = CGSize
+
+public extension CGMedidas {
+    var ancho: CGFloat { width }
+    var alto: CGFloat { height }
+}
