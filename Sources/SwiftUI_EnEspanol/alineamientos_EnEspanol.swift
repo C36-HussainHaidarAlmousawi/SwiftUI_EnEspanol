e601ad55da471d05e632b02cdc48e1b315029cc9
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2020-02-05 00:51:24 +0100

    `Alignment`, `HorizontalAlignment` y `VerticalAlignment` traducidos a `Alineamiento`, `AlineamientoHorizontal` y `AlineamientoVertical` respectivamente

---
+++ Sources/SwiftUI_EnEspanol/alineamientos_EnEspanol.swift
@@ -0,0 +1,10 @@
+//
+//  alineamientos_EnEspanol.swift
+//  
+//
+//  Created by Jeremy Bannister on 2/5/20.
+//
+
+public typealias Alineamiento = Alignment
+public typealias AlineamientoHorizontal = HorizontalAlignment
+public typealias AlineamientoVertical = VerticalAlignment
