a7f644204d32a0f712900b7b166f02bcce3bf6f4
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2020-02-05 01:04:21 +0100

    Los casos de los varios tipos de alineamiento están traducidos

--- Sources/SwiftUI_EnEspanol/alineamientos_EnEspanol.swift
+++ Sources/SwiftUI_EnEspanol/alineamientos_EnEspanol.swift
@@ -6,5 +6,31 @@
 //
 
 public typealias Alineamiento = Alignment
+
+public extension Alineamiento {
+    static var centro: Alineamiento { .center }
+    static var anterior: Alineamiento { .leading }
+    static var trasero: Alineamiento { .trailing }
+    static var arriba: Alineamiento { .top }
+    static var abajo: Alineamiento { .bottom }
+    static var arribaAnterior: Alineamiento { .topLeading }
+    static var arribaTrasero: Alineamiento { .topTrailing }
+    static var abajoAnterior: Alineamiento { .bottomLeading }
+    static var abajoTrasero: Alineamiento { .bottomTrailing }
+}
+
 public typealias AlineamientoHorizontal = HorizontalAlignment
+
+public extension AlineamientoHorizontal {
+    static var centro: AlineamientoHorizontal { .center }
+    static var anterior: AlineamientoHorizontal { .leading }
+    static var trasero: AlineamientoHorizontal { .trailing }
+}
+
 public typealias AlineamientoVertical = VerticalAlignment
+
+public extension AlineamientoVertical {
+    static var centro: AlineamientoVertical { .center }
+    static var arriba: AlineamientoVertical { .top }
+    static var abajo: AlineamientoVertical { .bottom }
+}
